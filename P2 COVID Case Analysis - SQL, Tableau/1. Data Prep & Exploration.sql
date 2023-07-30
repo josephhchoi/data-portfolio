237f93e5a8599c133383882bd2d1eec2c23d879c
Joseph Choi <127680630+josephhchoi@users.noreply.github.com>
2023-07-30 11:24:38 -0700

    Update 1. Data Prep & Exploration.sql

--- P2 COVID Case Analysis - SQL, Tableau/1. Data Prep & Exploration.sql
+++ P2 COVID Case Analysis - SQL, Tableau/1. Data Prep & Exploration.sql
@@ -1,10 +1,10 @@
 ----- SETUP -----
 
--- Ensuring that the 'coviddata' table imported successfully
+-- Ensuring that the 'coviddata' table imported successfully --
 SELECT *
 FROM portfolioproject.coviddata;
 
--- Updating the data types of certain columns to the correct type:
+-- Updating the data types of certain columns to the correct type --
 UPDATE portfolioproject.coviddata SET `date` = DATE_FORMAT(STR_TO_DATE(`date`, '%m/%d/%Y'), '%Y-%m-%d'); -- Converting the existing 'date' values to the correct format
 ALTER TABLE portfolioproject.coviddata MODIFY COLUMN `date` DATE; -- 'date' column: from TEXT to DATE
 
@@ -28,40 +28,40 @@ ALTER TABLE portfolioproject.coviddata MODIFY COLUMN `total_vaccinations` BIGINT
 
 ----- DATA EXPLORATION -----
 
--- 1.) Total Cases vs Total Deaths: Displays COVID-19 fatality probability by country
+-- 1.) Total Cases vs Total Deaths: Displays COVID-19 fatality probability by country -- 
 SELECT location, date, total_deaths, total_cases, (total_deaths/total_cases)*100 AS fatality_rate
 FROM portfolioproject.coviddata;
 ORDER BY 1,2;
 
--- 2.) Total Cases vs Population: Displays COVID-19 infection rates by country
+-- 2.) Total Cases vs Population: Displays COVID-19 infection rates by country --
 SELECT location, date, total_cases, population, (total_cases/population)*100 AS infection_rate
 FROM portfolioproject.coviddata;
 ORDER BY 1,2;
 
--- 3.) Countries w/ the Highest Infection Rate Compared to Population
+-- 3.) Countries w/ the Highest Infection Rate Compared to Population --
 SELECT location, MAX(total_cases) AS max_case_count, population, MAX((total_cases/population))*100 AS max_infection_rate
 FROM portfolioproject.coviddata
 GROUP BY location, population
 ORDER BY 4 DESC;
 
--- 4.) Countries w/ the Highest Death Count per Population
+-- 4.) Countries w/ the Highest Death Count per Population --
 SELECT location, sum(new_deaths) AS fatality_count
 FROM portfolioproject.coviddata
 GROUP BY location
 ORDER BY 2 DESC;
 
--- 5.) Continents w/ the Highest Death Count per Population
+-- 5.) Continents w/ the Highest Death Count per Population --
 SELECT continent, sum(new_deaths) AS fatality_count
 FROM portfolioproject.coviddata
 GROUP BY continent
 ORDER BY 2 DESC;
 
--- 6.) Global COVID-19 fatality probability by date
+-- 6.) Global COVID-19 fatality probability by date --
 SELECT date, SUM(new_deaths) AS ttl_deaths, SUM(new_cases) AS ttl_cases, SUM(new_deaths)/SUM(new_cases)*100 AS fatality_rate
 FROM portfolioproject.coviddata
 GROUP BY date;
 
--- 7.) Total Population vs Vaccinations: displaying the # of people vaccinated globally via 'CTE' & 'Temp Table'
+-- 7.) Total Population vs Vaccinations: displaying the # of people vaccinated globally via 'CTE' & 'Temp Table' --
 
 -- Using CTE --
 WITH PopvsVac (continent, location, date, population, new_vaccinations, rolling_vaccinnated_count)
@@ -96,7 +96,7 @@ ORDER BY 2, 3;
 SELECT *, (rolling_vaccinated_count/population) AS vaccinated_rate
 FROM PopulationVaccinatedRate;
 
--- 8.) Creating view for future visualizations
+-- 8.) Creating view for future visualizations --
 CREATE VIEW PopulationVaccinatedRate AS
 SELECT continent, location, date, population, new_vaccinations, 
 SUM(new_vaccinations) OVER (PARTITION BY location ORDER BY location, date) AS rolling_vaccinnated_count
